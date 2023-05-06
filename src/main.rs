1b61a2680927873ca0a5572c39041920fd765de0
Andrew Dionizio <andrew_dionizio@uri.edu>
2023-05-06 13:58:06 -0400

    removed last change

--- src/main.rs
+++ src/main.rs
@@ -8,17 +8,17 @@ fn main() {
 
     // Gets the instruction vector using rumload
     let instructions = rumload::load(input.as_deref());
-    let length = instructions.len();
+
 
     // Initializes Universal Machine
 
     let mut um = UniMachine {
         reg: [0; 8],
-        mem: Vec::with_capacity(length),
+        mem: vec![instructions],
         counter: 0,
         unmapped: Vec::new(),
     };
-    um.mem.push(instructions);
+
 
     // Loop through instructions
     while um.counter < um.mem[0].len() as u32 {
